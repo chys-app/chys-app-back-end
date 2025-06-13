1b0ff39f14c97ecf54230dfabcf5fdcef571f246
hmdmohsinali <hmdmohsinali@gmail.com>
2025-06-13 10:10:48 +0500

    bug fixed

--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Message = require('../models/Message');
-const auth = require('../middlewares/auth');
+const auth = require('../middleware/auth')
 
 // Get messages between the authenticated user and another user
 router.get('/:receiverId', auth, async (req, res) => {
