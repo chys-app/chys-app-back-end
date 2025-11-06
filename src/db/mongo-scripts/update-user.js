3d5873f172262857a939c03c6ae202a796dab0b1
Leo Pelekh <leopelekh@gmail.com>
2025-11-06 16:02:48 -0500

    Mongo Update User Example

---
+++ src/db/mongo-scripts/update-user.js
@@ -0,0 +1,11 @@
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// The current database to use.
+use("test");
+
+// Find a document in a collection.
+db.getCollection("users").updateOne(
+    { email: "40@40.com" },
+    { $set: { isVerified: true } }
+);
